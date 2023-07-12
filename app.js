e762b64f730ae6a2e61e6a7670c4daa510c4ba9b
abel3ri <abelmerete@yahoo.com>
2023-07-12 18:28:21 +0300

    Create anime class for holding all relevant data

--- app.js
+++ app.js
@@ -1,35 +1,64 @@
+// // prettier-ignore
+class Anime {
+  constructor(
+    imgSrc,
+    title,
+    releaseDate,
+    rating,
+    episodes,
+    genres,
+    episodeDuration,
+    synopsis
+  ) {
+    this.imgSrc = imgSrc;
+    this.title = title;
+    this.releaseDate = releaseDate;
+    this.rating = rating;
+    this.episodes = episodes;
+    this.genres = genres;
+    this.episodeDuration = episodeDuration;
+    this.synopsis = synopsis;
+  }
+  formatDate() {
+    return Intl.DateTimeFormat(navigator.language, {
+      month: "short",
+      year: "numeric",
+    }).format(new Date(this.releaseDate));
+  }
+
+  renderAnime() {
+    const animeContainer = document.querySelector(".container");
+    const html = `
+    <div class="anime">
+      <img src="${this.imgSrc}" alt="Anime image" class="anime-img" />
+      <div class="title-container"><p>Title</p><p>${this.title}</p></div>
+      <div class="released-container"><p>Rleased</p><p>${this.formatDate()}</p></div>
+      <div class="rating-container"><p>Rating</p><p>${this.rating}</p></div>
+    </div>`;
+
+    animeContainer.insertAdjacentHTML("beforeend", html);
+  }
+}
+
 async function getAnimeData(name) {
   const url = `https://api.jikan.moe/v4/anime?q=${name}&sfw`;
   const res = await fetch(url);
 
   const { data: animeData } = await res.json();
-  animeData.forEach((anime) => {
-    renderAnime(anime);
+  //   console.log(animeData[0]);
+  animeData.forEach((a) => {
+    const anime = new Anime(
+      a.images.webp.image_url,
+      a.title,
+      a.aired.from,
+      a.score,
+      a.episodes,
+      a.genres,
+      a.duration,
+      a.synopsis
+    );
+    anime.renderAnime();
   });
-}
-
-function formatDate(date) {
-  return Intl.DateTimeFormat(navigator.language, {
-    month: "short",
-    year: "numeric",
-  }).format(new Date(date));
-}
-
-function renderAnime(animeData) {
-  const animeContainer = document.querySelector(".container");
-  const html = `
-  <div class="anime">
-    <img src="${
-      animeData.images.webp.image_url
-    }" alt="Anime image" class="anime-img" />
-    <div class="title-container"><p>Title</p><p>${animeData.title}</p></div>
-    <div class="released-container"><p>Rleased</p><p>${formatDate(
-      animeData.aired.from
-    )}</p></div>
-    <div class="rating-container"><p>Rating</p><p>${animeData.score}</p></div>
-  </div>`;
-
-  animeContainer.insertAdjacentHTML("beforeend", html);
 }
 
 getAnimeData("Naruto");
